ec03787ea7b38bc069284928c41f0c259660498f
barbudonospes <barbudonospes@DESKTOP-412RIDH>
2021-11-28 22:38:10 +0300

    Добавьте файлы проекта.

---
+++ String_6_2_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_6_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите с клавиатуры предложение");
+            string startString = Console.ReadLine();
+            int b;
+            string[] word = startString.Split();
+            int[] a = new int[];
+            for (int i = 0; i < ; i++)
+            {
+                
+                    word.Length
+            }
+            {
+
+            }
+            
+        }
+    }
+}
